cc999d2461b896ae4ec8dd3fe7d14ad64b921e8d
Mark Dalire <mmdalire@gmail.com>
2020-05-12 19:44:20 +0800

    Creates the functionality to search for keywords and perform advanced filtering on both desktop and mobile devices

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 const wholeBody = document.querySelector('html');
 
+//Logo
+const logo = document.querySelector('.nav-logo');
+
 //Image slider
 const slider = document.querySelector('.image-slider');
 const next = document.querySelector('#right-btn');
@@ -10,8 +13,25 @@ const mobileNavOpen = document.querySelector('#bars');
 const mobileNavClose = document.querySelector('#close');
 const mobileNav = document.querySelector('.mobile-nav-bar');
 
-//Headline grid
+//Grids
 const headlineContainer = document.querySelector('.headline-grid');
+const everythingContainer = document.querySelector('.everything-grid');
+
+//Category
+const categoryList = document.querySelectorAll('[data-category]');
+
+//Keyword
+const keywordSearch = document.querySelector('#search-keyword');
+const keywordBtn = document.querySelector('#search-keyword-btn');
+const mobileKeywordSearch = document.querySelector('#mobile-search-keyword');
+const mobileKeywordBtn = document.querySelector('#mobile-keyword-btn');
+
+//Advanced search variables
+const domain = document.querySelector('#domains');
+const dateFrom = document.querySelector('#date-from');
+const dateUntil = document.querySelector('#date-until');
+const sortBy = document.querySelector('#sort-by');
+const advancedSearchBtn = document.querySelector('#advanced-search-btn');
 
 /*Image selector section*/
 const nextSlide = () => {
@@ -44,6 +64,7 @@ const automaticSlide = () => {
     setInterval(nextSlide, delay);
 }
 
+//Create the slide dynamically based on current news
 const createSlide = data => {
     const numberOfSlides = 5;
     for(let i = 0; i < numberOfSlides; i++) {
@@ -99,7 +120,16 @@ const formatDate = date => {
     return `${monthsArray[newDate.getMonth()]} ${newDate.getDate()}, ${newDate.getFullYear()} - ${parseZero(newDate.getHours(),newDate.getMinutes())}`;
 }
 
-const displayNews = data => {
+//Display the news in the grid whether it is top-headline or everything
+const displayNews = (data, endpoint) => {
+    //Container of variables 
+    const containers = [headlineContainer, everythingContainer];
+    let index;
+
+    //Change the variables depending on the endpoint
+    if(endpoint === 'top-headlines?') index = 0;
+    else index = 1;
+
     let displayNewsMaximum = (data.articles.length < 6) ? data.articles.length : 6;
 
     const loadMoreNews = document.createElement('button');
@@ -138,53 +168,191 @@ const displayNews = data => {
         newsCard.appendChild(newsCardThumbnail);
         newsCard.appendChild(newsCardInfo);
 
-        headlineContainer.appendChild(newsCard);
-        console.log(newsCard);
+        containers[index].appendChild(newsCard);
     }
-    data.articles.splice(0, displayNewsMaximum);
-    headlineContainer.appendChild(loadMoreNews);
+    data.articles.splice(0, displayNewsMaximum); //Remove the articles being showed
 
+    containers[index].appendChild(loadMoreNews);
+
+    //Removes the load more button whenever there are no news to show
     if(data.articles.length <= displayNewsMaximum) {
         displayNewsMaximum = data.articles.length;
-        if(data.articles.length === 0) headlineContainer.removeChild(loadMoreNews);
+        if(data.articles.length === 0) containers[index].removeChild(loadMoreNews);
     }
     
+    //Display more news when load more is clicked
     loadMoreNews.addEventListener('click', () => {
-        headlineContainer.removeChild(loadMoreNews);
-        displayNews(data);
+        containers[index].removeChild(loadMoreNews);
+        displayNews(data, endpoint);
     });
 }
 
+//Displays the active tab
+const addCategoryActive = category => {
+    const active = document.querySelector('.active-nav');
+    if(active === null) {
+        category.classList.add('active-nav');
+        return;
+    }
+    active.classList.remove('active-nav');
+    category.classList.add('active-nav');
+}
+
+//Remove active in headers
+const removeCategoryActive = () => {
+    const active = document.querySelector('.active-nav');
+    if(active !== null) active.classList.remove('active-nav');
+}
+
+const clearDisplay = () => {
+    document.querySelector('.image-slider').textContent = '';
+    document.querySelector('.headline-grid').textContent = '';
+    document.querySelector('.everything-grid').textContent = '';
+}
+
 /*News info*/
-const readNews = () => {
-    const url = 'js/sample.json';
+const readNews = (category, country, keyword, filter) => {
+    const apiKey = `apiKey=${key.NEWS_KEY}`;
+    const apiUrl = 'https://newsapi.org/v2/';
+    const apiCategory = (category.textContent === '' || category.textContent === undefined || category.textContent === null) ? '' : `&category=${category.textContent}`;
+    const apiCountry = (country === '' || country === undefined) ? '' : `&country=${country}`;
+    const apiKeyword = (keyword.value === '' || keyword.value === undefined) ? '' : `&q=${keyword.value}`;
+    const apiEndpoint = (keyword.value === '' || keyword.value === undefined) ? 'top-headlines?' : 'everything?';
+    let otherParameters = '';
+
+
+    //Added advanced filter parameters
+    if(filter !== undefined) {
+        if(filter[0] !== '' && filter[0] !== undefined) otherParameters += `&domain=${filter[0]}`;
+        if(filter[1] !== '' && filter[1] !== undefined) otherParameters += `&from=${filter[1]}`;
+        if(filter[2] !== '' && filter[2] !== undefined) otherParameters += `&to=${filter[2]}`;
+        if(filter[3] !== '' && filter[3] !== undefined && filter[3] !== 'none') otherParameters += `&sortBy=${filter[3]}`;
+    }
+
+    const url = `${apiUrl}${apiEndpoint}${apiKey}${apiCategory}${apiCountry}${apiKeyword}${otherParameters}`;
+    
+    clearDisplay(); //Clear display when switched categories
 
     fetch(url)
     .then(res => res.json())
     .then(data => {
         createSlide(data);
-        displayNews(data);
+        displayNews(data, apiEndpoint);
     })
     .catch(error => console.log(error))
 }
 
-automaticSlide();
-readNews();
+//Change displays
+const changeDisplays = device => {
+    //Change displays whether the user wants to view category or keyword
+    if(device.value === undefined || device.value === '') {
+        document.querySelector('.display-keyword').style.display = 'none';
+        document.querySelector('.display-category').style.display = 'block';
+    }
+    else {
+        document.querySelector('.display-keyword').style.display = 'block';
+        document.querySelector('.display-category').style.display = 'none';
+    }
+}
 
+automaticSlide();
+readNews('', 'ph', '', undefined);
+
+//Transfer page when logo is clicked
+logo.addEventListener('click', () => {
+    readNews('', 'ph', '', undefined);
+    removeCategoryActive();
+    //Remove contents in textbox
+    keywordSearch.value = '';
+    mobileKeywordSearch.value = '';
+});
+
+//Next slide in image slider
 next.addEventListener('click', () => {
     nextSlide();
 });
 
+//Previous slide in image slider
 previous.addEventListener('click', () => {
     prevSlide();
 });
 
+//Mobile navigation bar opens
 mobileNavOpen.addEventListener('click', () => {
     mobileNav.style.display = 'block';
     wholeBody.style.overflowY = 'hidden';
 });
 
+//Mobile navigation bar closes
 mobileNavClose.addEventListener('click', () => {
     mobileNav.style.display = 'none';
     wholeBody.style.overflowY = 'auto';
 });
+
+/*Nav bar*/
+categoryList.forEach(category => {
+    //Change the display of news based on category
+    category.addEventListener('click', () => {
+        if(document.querySelector('.display-keyword').style.display === 'block') {
+            document.querySelector('.display-keyword').style.display = 'none';
+            document.querySelector('.display-category').style.display = 'block';
+        }
+        addCategoryActive(category); //Make that category active 
+        readNews(category, 'ph', '', undefined); //Fetch the news data based on category
+
+        //Closes the navigation bar in mobile
+        mobileNav.style.display = 'none';
+        wholeBody.style.overflowY = 'auto';
+
+        //Remove contents in textbox
+        keywordSearch.value = '';
+        mobileKeywordSearch.value = '';
+    });
+});
+
+/*Keyword search*/
+keywordBtn.addEventListener('click', () => {
+    //Remove active in the links
+    removeCategoryActive();
+
+    //Change displays whether the user wants to view category or keyword
+    changeDisplays(keywordSearch); //For non-mobile version
+
+    //Closes the navigation bar in mobile
+    mobileNav.style.display = 'none';
+    wholeBody.style.overflowY = 'auto';
+
+    clearDisplay(); //Clears the display in the grid
+    readNews('', '', keywordSearch, undefined); //Fetch the news based on keywords
+});
+
+//Mobile search
+mobileKeywordBtn.addEventListener('click', () => {
+    //Remove active in the links
+    removeCategoryActive();
+
+    //Change displays whether top-headline or everything
+    changeDisplays(mobileKeywordSearch); //For mobile version
+
+    //Closes the navigation bar in mobile
+    mobileNav.style.display = 'none';
+    wholeBody.style.overflowY = 'auto';
+
+    clearDisplay(); //Clears the display in the grid
+    readNews('', '', mobileKeywordSearch, undefined); //Fetch the news based on keywords
+});
+
+//Advanced search filtering
+advancedSearchBtn.addEventListener('click', () => {
+    const isMobile = window.matchMedia('(max-width: 1020px)');    
+    const filterParameters = [domain.value, dateFrom.value, dateUntil.value, sortBy.value];
+
+    if(isMobile.matches) {
+        readNews('', '', mobileKeywordSearch, filterParameters);
+        keywordSearch.value = mobileKeywordSearch.value;
+    }
+    else {
+        readNews('', '', keywordSearch, filterParameters);
+        mobileKeywordSearch.value = keywordSearch.value;
+    }
+})
